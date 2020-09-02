92bfdbe81a54c02afe8e9f9243616b08bc58ceae
papino0319 <papino0319>
2020-09-02 17:29:45 +0900

    add makedir script and template file

---
+++ template/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int N;
+    string S;
+    cin >> S;
+
+    if(N){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
